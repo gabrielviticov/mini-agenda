de669e8d00dc9ed721142bf066dcbc6dfc159dd5
Gabriel Viticov Plata de Souza <gabrielviticov.ctt@gmail.com>
2023-06-06 08:00:08 -0300

    create a new project

---
+++ src/main/java/br/com/projeto/miniagenda/MiniAgendaApplication.java
@@ -0,0 +1,13 @@
+package br.com.projeto.miniagenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniAgendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/projeto/miniagenda/MiniAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.projeto.miniagenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniAgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
